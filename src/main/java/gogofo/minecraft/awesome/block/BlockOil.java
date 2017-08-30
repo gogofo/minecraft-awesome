285a1e4e7fc46552dd1d92685ac6bad186d1c92b
unknown <gogofo@gmail.com>
2017-08-30 23:49:05 +0300

    make oil flammable

--- src/main/java/gogofo/minecraft/awesome/block/BlockOil.java
+++ src/main/java/gogofo/minecraft/awesome/block/BlockOil.java
@@ -3,6 +3,7 @@ package gogofo.minecraft.awesome.block;
 import gogofo.minecraft.awesome.init.Fluids;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.block.statemap.StateMap;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fml.relauncher.Side;
@@ -11,6 +12,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class BlockOil extends BlockFluidClassic {
     public BlockOil() {
         super(Fluids.oil, Material.WATER);
+
+        Blocks.FIRE.setFireInfo(this, 60, 300);
     }
 
     @SideOnly(Side.CLIENT)
