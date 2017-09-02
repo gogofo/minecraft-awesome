d1fdb3d5d5282033f1d7e9265ab99c8162f6d222
unknown <gogofo@gmail.com>
2017-09-02 19:22:21 +0300

    generate straight to the src

--- ore-generation-template/generate.py
+++ ore-generation-template/generate.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
 
     for base_path,directories,files in files:
         for d in directories:
-            d_path = os.path.join(base_path, d).replace("template", ore_name)
+            d_path = os.path.join(base_path, d).replace("./ore-template", "..").replace("template", ore_name)
             
             if not os.path.exists(d_path):
                 os.makedirs(d_path)
                 
         for f in files:
             of_path = os.path.join(base_path, f)
-            f_path = of_path.replace("template", ore_name)
+            f_path = of_path.replace("./ore-template", "..").replace("template", ore_name)
 
             if not os.path.exists(f_path):
                 copy(of_path, f_path)
