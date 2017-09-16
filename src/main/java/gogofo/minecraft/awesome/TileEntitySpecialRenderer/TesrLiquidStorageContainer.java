a1aecd4c0c6ce478b89e72af41e9db1207bee570
unknown <gogofo@gmail.com>
2017-09-16 12:03:40 +0300

    draw white box

--- src/main/java/gogofo/minecraft/awesome/TileEntitySpecialRenderer/TesrLiquidStorageContainer.java
+++ src/main/java/gogofo/minecraft/awesome/TileEntitySpecialRenderer/TesrLiquidStorageContainer.java
@@ -1,7 +1,10 @@
 package gogofo.minecraft.awesome.TileEntitySpecialRenderer;
 
 import gogofo.minecraft.awesome.tileentity.TileEntityLiquidStorageContainer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import org.lwjgl.opengl.GL11;
 
 public class TesrLiquidStorageContainer extends TileEntitySpecialRenderer<TileEntityLiquidStorageContainer> {
 
@@ -9,6 +12,46 @@ public class TesrLiquidStorageContainer extends TileEntitySpecialRenderer<TileEn
     public void render(TileEntityLiquidStorageContainer te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
         super.render(te, x, y, z, partialTicks, destroyStage, alpha);
 
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x, y, z);
 
+        GlStateManager.color(0x255, 0x255, 0x255);
+        GlStateManager.disableTexture2D();
+        GlStateManager.disableCull();
+        GlStateManager.glBegin(GL11.GL_QUADS);
+
+        GlStateManager.glVertex3f(0, 0, 0);
+        GlStateManager.glVertex3f(1, 0, 0);
+        GlStateManager.glVertex3f(1, 1, 0);
+        GlStateManager.glVertex3f(0, 1, 0);
+
+        GlStateManager.glVertex3f(0, 0, 1);
+        GlStateManager.glVertex3f(1, 0, 1);
+        GlStateManager.glVertex3f(1, 1, 1);
+        GlStateManager.glVertex3f(0, 1, 1);
+
+        GlStateManager.glVertex3f(0, 0, 0);
+        GlStateManager.glVertex3f(1, 0, 0);
+        GlStateManager.glVertex3f(1, 0, 1);
+        GlStateManager.glVertex3f(0, 0, 1);
+
+        GlStateManager.glVertex3f(0, 1, 0);
+        GlStateManager.glVertex3f(1, 1, 0);
+        GlStateManager.glVertex3f(1, 1, 1);
+        GlStateManager.glVertex3f(0, 1, 1);
+
+        GlStateManager.glVertex3f(0, 0, 0);
+        GlStateManager.glVertex3f(0, 0, 1);
+        GlStateManager.glVertex3f(0, 1, 1);
+        GlStateManager.glVertex3f(0, 1, 0);
+
+        GlStateManager.glVertex3f(1, 0, 0);
+        GlStateManager.glVertex3f(1, 0, 1);
+        GlStateManager.glVertex3f(1, 1, 1);
+        GlStateManager.glVertex3f(1, 1, 0);
+
+        GlStateManager.glEnd();
+
+        GlStateManager.popMatrix();
     }
 }
