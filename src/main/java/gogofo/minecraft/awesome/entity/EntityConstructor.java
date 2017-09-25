e888d3168f982bdd91a4f9dc56182715ab11f229
unknown <gogofo@gmail.com>
2017-09-25 18:05:15 +0300

    set a bounding box and collision for the entity

--- src/main/java/gogofo/minecraft/awesome/entity/EntityConstructor.java
+++ src/main/java/gogofo/minecraft/awesome/entity/EntityConstructor.java
@@ -2,13 +2,17 @@ package gogofo.minecraft.awesome.entity;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class EntityConstructor extends Entity {
 
     public EntityConstructor(World world) {
         super(world);
+        setSize(1, 1);
     }
 
     public EntityConstructor(World worldIn, BlockPos pos) {
@@ -29,5 +33,25 @@ public class EntityConstructor extends Entity {
     @Override
     protected void writeEntityToNBT(NBTTagCompound compound) {
 
+    }
+
+    @Nullable
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox() {
+        return getEntityBoundingBox();
+    }
+
+    @Nullable
+    @Override
+    public AxisAlignedBB getCollisionBox(Entity entityIn) {
+        return getEntityBoundingBox();
+    }
+
+    @Override
+    public void setPosition(double x, double y, double z) {
+        this.posX = x;
+        this.posY = y;
+        this.posZ = z;
+        this.setEntityBoundingBox(new AxisAlignedBB(x, y, z, x + width, y + height, z + width));
     }
 }
