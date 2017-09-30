d569442e477485959bada37dce267408bb2ea36f
unknown <gogofo@gmail.com>
2017-10-01 01:25:32 +0300

    add rubber recipe with buckets

---
+++ src/main/java/gogofo/minecraft/awesome/recipe/custom/AwesomeIngredientNBT.java
@@ -0,0 +1,11 @@
+package gogofo.minecraft.awesome.recipe.custom;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.crafting.IngredientNBT;
+
+public class AwesomeIngredientNBT extends IngredientNBT {
+
+    public AwesomeIngredientNBT(ItemStack stack) {
+        super(stack);
+    }
+}
---
+++ src/main/java/gogofo/minecraft/awesome/recipe/custom/FilledUniversalBucketIngredientFactory.java
@@ -0,0 +1,43 @@
+package gogofo.minecraft.awesome.recipe.custom;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.JsonUtils;
+import net.minecraftforge.common.crafting.IIngredientFactory;
+import net.minecraftforge.common.crafting.IngredientNBT;
+import net.minecraftforge.common.crafting.JsonContext;
+import net.minecraftforge.fluids.*;
+
+/**
+ * An ingredient factory that produces a {@link UniversalBucket} filled with the specified {@link Fluid}.
+ * <p>
+ * JSON Properties:
+ * <ul>
+ * <li><code>fluid</code> - The name of the {@link Fluid} to fill the bucket with</li>
+ * </ul>
+ *
+ * @author Choonster
+ */
+@SuppressWarnings("unused")
+public class FilledUniversalBucketIngredientFactory implements IIngredientFactory {
+
+    @Override
+    public Ingredient parse(final JsonContext context, final JsonObject json) {
+        final String fluidName = JsonUtils.getString(json, "fluid");
+        final Fluid fluid = FluidRegistry.getFluid(fluidName);
+
+        if (fluid == null) {
+            throw new JsonSyntaxException("Unknown fluid '" + fluidName + "'");
+        }
+
+        final ItemStack filledBucket = FluidUtil.getFilledBucket(new FluidStack(fluid, 0));
+
+        if (filledBucket.isEmpty()) {
+            throw new JsonSyntaxException("No bucket registered for fluid '" + fluidName + "'");
+        }
+
+        return new AwesomeIngredientNBT(filledBucket);
+    }
+}
