1ff939302d29f4274f116a9da93edd5784499eb2
unknown <gogofo@gmail.com>
2017-10-02 12:06:08 +0300

    enable air as a fluid type for recipes in containers

--- src/main/java/gogofo/minecraft/awesome/recipe/custom/FilledLiquidContainerIngredientFactory.java
+++ src/main/java/gogofo/minecraft/awesome/recipe/custom/FilledLiquidContainerIngredientFactory.java
@@ -4,6 +4,8 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
 import gogofo.minecraft.awesome.init.Items;
 import gogofo.minecraft.awesome.item.ItemLiquidContainer;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.JsonUtils;
@@ -11,20 +13,28 @@ import net.minecraftforge.common.crafting.IIngredientFactory;
 import net.minecraftforge.common.crafting.JsonContext;
 import net.minecraftforge.fluids.*;
 
+import java.util.Objects;
+
 @SuppressWarnings("unused")
 public class FilledLiquidContainerIngredientFactory implements IIngredientFactory {
 
     @Override
     public Ingredient parse(final JsonContext context, final JsonObject json) {
         final String fluidName = JsonUtils.getString(json, "fluid");
-        final Fluid fluid = FluidRegistry.getFluid(fluidName);
+        Block fluidBlock = Blocks.AIR;
 
-        if (fluid == null) {
-            throw new JsonSyntaxException("Unknown fluid '" + fluidName + "'");
+        if (!fluidName.equals("air")) {
+            final Fluid fluid = FluidRegistry.getFluid(fluidName);
+
+            if (fluid == null) {
+                throw new JsonSyntaxException("Unknown fluid '" + fluidName + "'");
+            }
+
+            fluidBlock = fluid.getBlock();
         }
 
         ItemStack stack = new ItemStack(Items.liquid_container);
-        ItemLiquidContainer.setLiquidType(stack, fluid.getBlock());
+        ItemLiquidContainer.setLiquidType(stack, fluidBlock);
 
         return new IngredientLiquidContainer(stack);
     }
